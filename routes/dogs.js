8235bbfd8c0cbee905461271fac83b8291d35f61
Phillip Cunningham <pmatt1988@gmail.com>
2020-03-10 06:58:33 -0500

    Added Dogs Router and dogs/index view

--- routes/dogs.js
+++ routes/dogs.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var Dog = require('../models/Dog');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', null);
+  res.render('dogs/index', null);
 });
 
 module.exports = router;
